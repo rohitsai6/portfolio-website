193c69831723e721015881e86174ded681482707
rohitsai <65433407+rohitsai6@users.noreply.github.com>
2022-10-02 15:27:01 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          backgroundColor: "aqua",
+          textDecoration: "underline",
+        }}
+      >
+        Rohit Sai Portfolio
+      </h1>
+    </div>
+  );
+}
+
+export default App;
